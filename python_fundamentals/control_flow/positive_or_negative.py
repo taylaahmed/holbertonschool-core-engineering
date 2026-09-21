d13f19b897561ce9db10f3c03f9e93e5bdbf5a9b
taylaahmed <14827@holbertonstudents.com>
2026-09-21 16:34:14 +1000

    Task 00 - Pos v. Neg - 00

---
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number = __import__('random').randint(-10, 10)
+
+if number > 0:
+    print(number, "is positive")
+    
+elif number == 0:
+    print(f"{number} is zero")
+
+else:
+    print(f"{number} is negative")
