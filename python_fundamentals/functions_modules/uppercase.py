d4ac6eb495f4820721a9b7550833299a1b9bd613
taylaahmed <14827@holbertonstudents.com>
2026-09-21 17:53:01 +1000

    Task 01 - uppercase - 00

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+
+    word = list(str)
+    new = []
+
+    for letter in word:
+        ascii_value = ord(letter)
+        if ascii_value >= 97 and ascii_value <= 122:
+            ascii_value -= 32
+            new.append(chr(ascii_value))
+        else:
+            new.append(letter)
+
+    uppercase = "".join(new)
+    print(uppercase)
