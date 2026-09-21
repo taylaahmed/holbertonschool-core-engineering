e1a7dd900cce55057e134ec009fe3dd6d6e00dbb
taylaahmed <14827@holbertonstudents.com>
2026-09-21 20:59:56 +1000

    Task 03 - pw - 01 Fix: negetive number pow

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -3,8 +3,12 @@
 def pow(a, b):
     i = 0
     total = 1
-    while i < b:
-        total *= a
-        i += 1
-
+    if b > 0:
+        while i < b:
+            total *= a
+            i += 1
+    else:
+        while i > b:
+            total /= a
+            i -= 1
     return total
