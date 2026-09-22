7241176322ba790fbb81e0c6a2ab38e40e13df30
taylaahmed <14827@holbertonstudents.com>
2026-09-22 11:52:21 +1000

    Task 05 - print comb 3 - 00

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+i = 0
+sep = ""
+
+
+while i < 100:
+    j = 0
+    unique = True
+    while j < i:
+        numComp = sorted("{:02d}".format(j))
+        original = sorted("{:02d}".format(i))
+
+        if numComp == original or original[0] == original[1]:
+            unique = False
+        j += 1
+
+    if unique:
+        print("{}{:02d}".format(sep, i), end="")
+        sep = ", "
+
+    i += 1
+
+print("\n", end="")
