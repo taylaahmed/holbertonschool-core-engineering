d791ed3dc448701af91cdb85626b38115bbc33de
taylaahmed <14827@holbertonstudents.com>
2026-09-22 10:41:32 +1000

    Task 03 - print matrix - 00

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for lines in matrix:
+        sep = ""
+        for number in lines:
+            print("{}{}".format(sep, number), end="")
+            sep = " "
+        print("\n", end="")
